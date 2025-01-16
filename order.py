cb53192b1e96dc060f22f54f3a98939be85254a3
hkandaswamy <77952068+hkandaswamy@users.noreply.github.com>
2025-01-16 08:58:11 -0800

    completed random generation from order class

--- order.py
+++ order.py
@@ -1,16 +1,42 @@
 import random as rand
 import datetime
+import radar
 
 class Order:
-    orderid = 1
+    id = 1
+    ecommerce_websites = [
+    "www.cvs.com",
+    "www.walgreens.com",
+    "www.riteaid.com",
+    "www.goodrx.com",
+    "www.healthmart.com",
+    "www.chemistwarehouse.com.au",
+    "www.pharmacytimes.com",
+    "www.drugstore.com",
+    "www.amazon.com/pharmacy",
+    "www.alldaychemist.com",
+    "www.iherb.com",
+    "www.1mg.com",
+    "www.pillpack.com",
+    "www.medicalnewstoday.com/pharmacy",
+    "www.expresspharmacy.co.uk",
+    "www.safeway.com/pharmacy",
+    "www.britishpharmacy.co.uk",
+    "www.walmart.com/pharmacy",
+    "www.mysmartpharmacy.com",
+    "www.chemistdirect.co.uk"
+]
 
     def __init__(self):
-        self.__orderid = Order.orderid
-        Order.orderid += 1
+        self.__orderid = Order.id
+        Order.id += 1
         self.__qty = rand.randint(1,10)
-        self.__timestamp = datetime.datetime.now()
-        self.__ecommercename = ""
-        print("Order info created")
+        self.__timestamp = radar.random_datetime(
+            start = datetime.datetime(year=2024, month=1, day=1),
+            stop = datetime.datetime(year=2024, month=12, day=31)
+        )
+        random_ecommerce = rand.randint(0,19)
+        self.__ecommercename = Order.ecommerce_websites[random_ecommerce]
 
     @property
     def orderid(self):
---
+++ testing.py
@@ -0,0 +1,7 @@
+#ignore this, will be deleted eventually
+
+import order
+
+for x in range(0, 35):
+        test = order.Order()
+        print("Loop: " + str(x) + " | " + str(test.orderid) + " " + str(test.qty) + " " + str(test.timestamp) + " " + test.ecommercename)
