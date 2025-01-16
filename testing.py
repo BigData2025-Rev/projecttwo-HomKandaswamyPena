55bdea9adff76bf49009eb268167eb7ded3a16b6
hkandaswamy <77952068+hkandaswamy@users.noreply.github.com>
2025-01-16 10:41:41 -0800

    deleted testing file

--- testing.py
+++
@@ -1,7 +0,0 @@
-#ignore this, will be deleted eventually
-
-import order
-
-for x in range(0, 35):
-        test = order.Order()
-        print("Loop: " + str(x) + " | " + str(test.orderid) + " " + str(test.qty) + " " + str(test.timestamp) + " " + test.ecommercename)
