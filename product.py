3d829cba7b40ae272d791ac4bee592d4fbb56c21
miguelpena-bigdata-dev <miguel674@revature.net>
2025-01-15 21:54:06 -0700

    product class finished, loads from json file and randomly selects a product when creating an object.

--- product.py
+++ product.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 from spark_singleton import SparkSingleton
+from pyspark.sql import DataFrame
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
 from abc import ABC, abstractmethod
 
@@ -15,19 +16,33 @@ class IProduct(ABC):
     @abstractmethod
     def get_dataframe(self):
         """
-            Returns a Spark DataFrame with product information.
+            Returns a Spark DataFrame with product information. Can be unioned or joined with other dataframes.
         """
         pass
+
     @staticmethod
     def get_normalized_rnd_integer(n):
+        """
+            Returns a random integer based on a normal distribution.
+            The integer is clipped to the range of [0, n - 1].
+            Args:
+                n (int): The upper bound of the range.
+            Returns:
+                int: A random integer in the range of [0, n - 1].
+        """
         mean = n / 2
         std_dev = n / 4
         dim_size = 1
-        random_integer = np.clip(np.round(np.random.normal(mean, std_dev, dim_size).astype(int)), 0, n - 1)
+        random_integer = np.clip(np.round(np.random.normal(mean, std_dev, dim_size)).astype(int), 0, n - 1)
         return random_integer[0]
     
     @staticmethod
     def load_products():
+        """
+            Loads the products from the json file.
+            Returns:
+                list: A list of products.
+        """
         with open(PRODUCTS_FILE, "r") as file:
             products = json.load(file)
         return products
@@ -36,6 +51,9 @@ class Product(IProduct):
     products = IProduct.load_products()
 
     def __init__(self):
+        """
+            Initializes the product object with random properties
+        """
         rnd_index = Product.get_normalized_rnd_integer(len(Product.products))
         self._name = Product.products[rnd_index].get("name")
         self._price = Product.products[rnd_index].get("price")
@@ -59,11 +77,19 @@ def test_product_creation():
         Test function to check if the product object is created correctly.
         Make sure it is run from the root directory. 
         It should print a dataframe with a single row, containing product information from a randomly selected product.
+        NOTE: It takes a long time to run, as it creates 1000 product dataframes, and unions them together. 
+        Let me know if you have any optimization suggestions.
     """
-    for i in range(10):
+    main_df: DataFrame = None
+    for i in range(1000):
         product = Product()
         df = product.get_dataframe()
-        df.show()
+        if main_df is None:
+            main_df = df
+        else:
+            main_df = main_df.union(df)
+    
+    main_df.show(main_df.count(), truncate=False)
 
 if __name__ == "__main__":
     test_product_creation()
