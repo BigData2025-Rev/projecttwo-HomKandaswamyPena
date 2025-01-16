830b6017a5df093ba8443bbda36a864f89ca7385
miguelpena-bigdata-dev <miguel674@revature.net>
2025-01-16 13:54:23 -0700

    switched to using list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from spark_singleton import SparkSingleton
 from pyspark.sql import DataFrame
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, TimestampType
+from pyspark.sql.types import Row, StructType, StructField, StringType, IntegerType, FloatType, TimestampType
 from product import Product
 from transaction import Transaction
 from order import Order
@@ -11,69 +11,30 @@ class OrderGenerator:
     rogue_options = ["ROGUEROGUE", "XXXXXXXX", "", "----////----"]
 
     def get_row():
-        spark_session = SparkSingleton.getInstance()
-        schema = StructType([
-            StructField("order_id", IntegerType(), True),
-            StructField("customer_id", IntegerType(), True),
-            StructField("customer_name", StringType(), True),
-            StructField("product_id", IntegerType(), True),
-            StructField("product_name", StringType(), True),
-            StructField("product_category", StringType(), True),
-            StructField("payment_type", StringType(), True),
-            StructField("qty", IntegerType(), True),
-            StructField("price", FloatType(), True),
-            StructField("datetime", TimestampType(), True),
-            StructField("country", StringType(), True),
-            StructField("city", StringType(), True),
-            StructField("ecommerce_website_name", StringType(), True),
-            StructField("payment_txn_id", IntegerType(), True),
-            StructField("payment_txn_success", StringType(), True),
-            StructField("failure_reason", StringType(), True)
-        ])
         product = Product()
         transaction = Transaction()
         order = Order()
         customer = Customer()
-        df = spark_session.createDataFrame([(order.orderid, 
-                                             customer.id, 
-                                             customer.name, 
-                                             product.id, 
-                                             product.name, 
-                                             product.category,
-                                             transaction.paymentType, 
-                                             order.qty, 
-                                             product.price, 
-                                             order.timestamp,
-                                             customer.country,
-                                             customer.city,
-                                             order.ecommercename,
-                                             transaction.paymentTxnId,
-                                             transaction.paymentTxnSuccess,
-                                             transaction.failureReason)], 
-                                             schema=schema)
+        row = Row(order.orderid, 
+                   customer.id, 
+                   customer.name, 
+                   product.id, 
+                   product.name,
+                   product.category,
+                   transaction.paymentType,
+                   order.qty,
+                   product.price, 
+                   order.timestamp,
+                   customer.country,
+                   customer.city,
+                   order.ecommercename,
+                   transaction.paymentTxnId,
+                   transaction.paymentTxnSuccess,
+                   transaction.failureReason)
         
-        return df
+        return row
     
     def get_rogue_row():
-        spark_session = SparkSingleton.getInstance()
-        schema = StructType([
-            StructField("order_id", IntegerType(), True),
-            StructField("customer_id", IntegerType(), True),
-            StructField("customer_name", StringType(), True),
-            StructField("product_id", IntegerType(), True),
-            StructField("product_name", StringType(), True),
-            StructField("product_category", StringType(), True),
-            StructField("payment_type", StringType(), True),
-            StructField("qty", IntegerType(), True),
-            StructField("price", FloatType(), True),
-            StructField("datetime", TimestampType(), True),
-            StructField("country", StringType(), True),
-            StructField("city", StringType(), True),
-            StructField("ecommerce_website_name", StringType(), True),
-            StructField("payment_txn_id", IntegerType(), True),
-            StructField("payment_txn_success", StringType(), True),
-            StructField("failure_reason", StringType(), True)
-        ])
         product = Product()
         transaction = Transaction()
         order = Order()
@@ -98,17 +59,36 @@ class OrderGenerator:
         rogue_row[5] = OrderGenerator.rogue_options[rand.randint(0,3)]
         rogue_row[10] = OrderGenerator.rogue_options[rand.randint(0,3)]
         rogue_row[15] = OrderGenerator.rogue_options[rand.randint(0,3)]
-        df = spark_session.createDataFrame([rogue_row], 
-                                             schema=schema)
         
-        return df
+        return tuple(rogue_row)
+
+def get_schema():
+    schema = StructType([
+            StructField("order_id", IntegerType(), True),
+            StructField("customer_id", IntegerType(), True),
+            StructField("customer_name", StringType(), True),
+            StructField("product_id", IntegerType(), True),
+            StructField("product_name", StringType(), True),
+            StructField("product_category", StringType(), True),
+            StructField("payment_type", StringType(), True),
+            StructField("qty", IntegerType(), True),
+            StructField("price", FloatType(), True),
+            StructField("datetime", TimestampType(), True),
+            StructField("country", StringType(), True),
+            StructField("city", StringType(), True),
+            StructField("ecommerce_website_name", StringType(), True),
+            StructField("payment_txn_id", IntegerType(), True),
+            StructField("payment_txn_success", StringType(), True),
+            StructField("failure_reason", StringType(), True)
+        ])
 
 def main():
     numRows = int(input("Enter number of records to generate: "))
     name = input("Name of .csv file to write to: ")
-    
-    df = OrderGenerator.get_row()
-
+    spark_session = SparkSingleton.getInstance()
+    row_list=[]
+    row = OrderGenerator.get_row()
+    row_list.append(row)
     for i in range(numRows-1):
 
         rng = rand.random()
@@ -117,12 +97,14 @@ def main():
             row = OrderGenerator.get_rogue_row()
         else: 
             row = OrderGenerator.get_row()
+        
+        row_list.append(row)
+        # df = df.union(row)
 
-        df = df.union(row)
-
+    df = spark_session.createDataFrame(row_list, get_schema())
     # df.show()
 
-    df.coalesce(1).write.csv(f'{name}.csv', header=True, mode='overwrite')
+    df.repartition(1).write.csv(f'{name}.csv', header=True, mode='overwrite')
 
 
 
