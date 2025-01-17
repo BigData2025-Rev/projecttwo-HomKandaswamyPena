22e9c3607dc032de486a67cd11e7e846ee258e61
miguelpena-bigdata-dev <miguel674@revature.net>
2025-01-17 09:01:52 -0700

    added seasonal products feature

--- product.py
+++ product.py
@@ -10,16 +10,32 @@ PRODUCTS_FILE = "products.json"
 class Product(RandomJSONSelector):
     products = RandomJSONSelector.load_list(PRODUCTS_FILE)
 
-    def __init__(self):
+    def __init__(self, season):
         """
             Initializes the product object with random properties
         """
-        rnd_index = Product.get_normalized_rnd_integer(len(Product.products))
-        self.__name = Product.products[rnd_index].get("name")
-        self.__price = Product.products[rnd_index].get("price")
-        self.__category = Product.products[rnd_index].get("category")
-        self.__id = Product.products[rnd_index].get("id")
+        self.products = self.get_seasonal_products(season)
+        rnd_index = Product.get_normalized_rnd_integer(len(self.products))
+        self.__name = self.products[rnd_index].get("name")
+        self.__price = self.products[rnd_index].get("price")
+        self.__category = self.products[rnd_index].get("category")
+        self.__id = self.products[rnd_index].get("id")
     
+    def get_seasonal_products(season):
+        """
+            Returns a list of products that are in the specified season
+        """
+        seasonal_categories = {
+            'summer':[],
+            'winter':["Shaving and Grooming", "Feminine Care", "Cough, Cold, and Flu"],
+            'spring':[],
+            'fall':[]
+        }
+        current_season = seasonal_categories.get(season)
+        seasonal_products = [product for product in Product.products if product.get("category") in current_season ]
+        
+        return seasonal_products
+
     @property
     def id(self):
         return self.__id
