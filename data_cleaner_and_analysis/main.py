9ee8992e745d5f58eaf171b8b61c33758628ffb9
miguelpena-bigdata-dev <miguel674@revature.net>
2025-01-21 08:42:15 -0700

    fixed issue, didn't import config constant

--- data_cleaner_and_analysis/main.py
+++ data_cleaner_and_analysis/main.py
@@ -1,10 +1,10 @@
 from spark_singleton import SparkSingleton
 
-
+from config import HDFS_DATA_DIR
 
 def main():
     spark = SparkSingleton.getInstance()
-    data_df = spark.read.csv("data/P2datafile.csv", header=True, inferSchema=True)
+    data_df = spark.read.csv(HDFS_DATA_DIR, header=True, inferSchema=True)
     data_df.show()
 
 if __name__ == "__main__":
