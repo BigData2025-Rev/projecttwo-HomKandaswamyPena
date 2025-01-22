6e29fb1f00e853de2a1765d56a5b15655a784e93
hkandaswamy <77952068+hkandaswamy@users.noreply.github.com>
2025-01-22 10:25:45 -0800

    completed all popular times analyzer

--- data_cleaner_and_analysis/popular_times.py
+++ data_cleaner_and_analysis/popular_times.py
@@ -1,19 +1,19 @@
+from pyspark.sql import functions
+
 class PopularTimes:
 
     def __init__(self, data):
         self.__data = data
 
     def popular_times_overall(self):
-        hour = 0
-        period = 'placeholder'
-        if 6 <= hour < 12:
-            period = 'morning'
-        elif 12 <= hour < 18:
-            period = 'afternoon'
-        elif 18 <= hour < 24:
-            period = 'evening'
-        else:
-            period = 'off-hours'
+        times_df = self.__data.withColumn('hour', hour(self.__data.datetime))
+        times_df = times_df.withColumn('period',
+                                 when((col('hour') >= 6) & (col('hour') < 12), "morning")
+                                .when((col('hour') >= 12) & (col('hour') < 18), "afternoon")
+                                .when((col('hour') >= 18) & (col('hour') < 24), "evening")
+                                .otherwise('off-hours'))
+        times_df = times_df.groupBy('period').count().orderBy('count', ascending=False)
+        return times_df
 
     def popular_times_countries(self):
         print('placeholder')
