9bfedfa7a821c05aec4c9d638c921935d1de5288
hkandaswamy <77952068+hkandaswamy@users.noreply.github.com>
2025-01-22 10:02:33 -0800

    removed sparksession from locations and times classes

--- data_cleaner_and_analysis/popular_locations.py
+++ data_cleaner_and_analysis/popular_locations.py
@@ -1,7 +1,4 @@
-import spark_singleton as ss
-
 class PopularLocations:
-    spark = ss.SparkSingleton.getInstance()
 
     def __init__(self, data):
         self.__data = data
--- data_cleaner_and_analysis/popular_times.py
+++ data_cleaner_and_analysis/popular_times.py
@@ -1,13 +1,19 @@
-import spark_singleton as ss
-
 class PopularTimes:
-    spark = ss.SparkSingleton.getInstance()
 
     def __init__(self, data):
         self.__data = data
 
     def popular_times_overall(self):
-        print('placeholder')
+        hour = 0
+        period = 'placeholder'
+        if 6 <= hour < 12:
+            period = 'morning'
+        elif 12 <= hour < 18:
+            period = 'afternoon'
+        elif 18 <= hour < 24:
+            period = 'evening'
+        else:
+            period = 'off-hours'
 
     def popular_times_countries(self):
         print('placeholder')
