8b22af59d1db783e333e13fb7135ff3d3783c4f5
hkandaswamy <77952068+hkandaswamy@users.noreply.github.com>
2025-01-22 08:22:23 -0800

    completed popular locations analyzer

--- data_cleaner_and_analysis/popular_locations.py
+++ data_cleaner_and_analysis/popular_locations.py
@@ -5,4 +5,9 @@ class PopularLocations:
 
     def __init__(self, data):
         self.__data = data
-    
+
+    def get_popular_countries(self):
+        return self.__data.groupBy('country').count().orderBy('count', ascending=False)
+
+    def get_popular_cities(self):
+        return self.__data.groupBy('city', 'country').count().orderBy('count', ascending=False)
