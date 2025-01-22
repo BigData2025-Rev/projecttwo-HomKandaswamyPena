7698bd4028b0ef80159d6967a0584e351afb2ed5
hkandaswamy <77952068+hkandaswamy@users.noreply.github.com>
2025-01-22 12:32:33 -0800

    fixed pyspark functions bug

--- data_cleaner_and_analysis/popular_times.py
+++ data_cleaner_and_analysis/popular_times.py
@@ -8,15 +8,15 @@ class PopularTimes:
     def get_popular_times_overall(self):
         times_df = self.__data.withColumn('period',
                                 F.when((F.col('hour') >= 6) & (F.col('hour') < 12), "morning")
-                                .F.when((F.col('hour') >= 12) & (F.col('hour') < 18), "afternoon")
-                                .F.when((F.col('hour') >= 18) & (F.col('hour') < 24), "evening")
+                                .when((F.col('hour') >= 12) & (F.col('hour') < 18), "afternoon")
+                                .when((F.col('hour') >= 18) & (F.col('hour') < 24), "evening")
                                 .otherwise('off-hours'))
         return times_df.groupBy('period').count().orderBy('count', ascending=False)
 
     def get_popular_times_countries(self):
         times_df = self.__data.withColumn('period',
                                 F.when((F.col('hour') >= 6) & (F.col('hour') < 12), "morning")
-                                .F.when((F.col('hour') >= 12) & (F.col('hour') < 18), "afternoon")
-                                .F.when((F.col('hour') >= 18) & (F.col('hour') < 24), "evening")
+                                .when((F.col('hour') >= 12) & (F.col('hour') < 18), "afternoon")
+                                .when((F.col('hour') >= 18) & (F.col('hour') < 24), "evening")
                                 .otherwise('off-hours'))
         return times_df.groupBy('country', 'period').count().orderBy('count', ascending=False)
