d3c8ee8556f8d7c401cfcc7d31a311727173a1d2
miguelpena-bigdata-dev <miguel674@revature.net>
2025-01-22 08:37:13 -0700

    added README details

--- data_cleaner_and_analysis/popularity_of_products.py
+++ data_cleaner_and_analysis/popularity_of_products.py
@@ -1,11 +1,16 @@
 import spark_singleton as ss
+from pyspark.sql import DataFrame
 
 class ProductPopularity():
     spark = ss.SparkSingleton.getInstance()
     
     def __init__(self, data):
         self.__data = data
-
-    def get_results(self):
-        data = self.__data
+    
+    @property
+    def data(self):
+        return self.__data
+    
+    def get_popularity_over_year(self):
+        data: DataFrame = self.__data
         return data.groupBy('product_id').count().orderBy('count', ascending=False)
